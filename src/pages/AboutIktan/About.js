0380c841364a86605ab9eefb10902888480dabcd
ArmandoArV <armando.arredondo.valle@gmail.com>
2022-11-20 14:56:08 -0600

    CreationOfHeader

---
+++ src/pages/AboutIktan/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../components/header/Header";
+
+function About() {
+    return (
+        <div className="AboutUs">
+            <Header />
+        </div>
+    );
+}
+
+export default About;
